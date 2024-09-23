0964f13b043ef0523348cd6075a84eee5654f4aa
G1org1owo <77230371+G1org1owo@users.noreply.github.com>
2024-09-23 23:13:24 +0200

    Introduce command code constants and handle unrecognized command code

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,10 @@
+use std::fmt::format;
 use serde::Serialize;
 use crate::byte_utils::{as_u32_be};
 use crate::show_message_command::ShowMessageCommand;
+
+const SHOW_MESSAGE_COMMAND: u32 = 0x01650000;
+const EXIT_COMMAND: u32 = 0x01000000;
 
 #[derive(Serialize)]
 pub enum Command {
@@ -13,16 +17,25 @@ impl Command {
     pub fn parse(bytes: &[u8]) -> (usize, Self) {
         let mut offset: usize = 0;
         let command: Command = match as_u32_be(&bytes[offset..offset+4]) {
-            0x01650000 => { // Comment and Debug Text have similar codes
+            SHOW_MESSAGE_COMMAND => { // Comment and Debug Text have similar codes
                 offset+=4;
                 let (bytes_read, command) : (usize, ShowMessageCommand)
                     = ShowMessageCommand::parse(&bytes[offset..]);
                 offset += bytes_read;
 
-                Command::ShowMessage(command)
+                Ok(Command::ShowMessage(command))
             },
-            _ => {Command::Exit()}
-        };
+            EXIT_COMMAND => {
+                offset+=4;
+                offset+=8; // Not sure what the contents of the EXIT command are at the moment
+
+                Ok(Command::Exit())
+            }
+            _ => Err("Unknown command code")
+        }.expect(format(format_args!(
+            "Could not interpret command 0x{:08x}",
+            as_u32_be(&bytes[offset..offset+4])
+        )).as_str());
 
         (offset, command)
     }
