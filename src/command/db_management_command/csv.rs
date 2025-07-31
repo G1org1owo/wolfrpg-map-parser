21f21c20c93069daa261ae58779d772b056db6cc
G1org1owo <77230371+G1org1owo@users.noreply.github.com>
2025-07-31 23:59:24 +0200

    Fix DBManagement parsing failing for rare 0-string command variant

--- src/command/db_management_command/csv.rs
+++ src/command/db_management_command/csv.rs
@@ -1,6 +1,9 @@
 use crate::byte_utils::{as_u32_le, parse_string};
 #[cfg(feature = "serde")]
 use serde::{Serialize, Deserialize};
+use crate::command::db_management_command::state::State;
+
+type DBStrings = (Option<String>, Option<String>, Option<String>);
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(PartialEq, Clone)]
@@ -10,7 +13,7 @@ pub struct CSV {
 }
 
 impl CSV {
-    pub(crate) fn parse(bytes: &[u8]) -> (usize, Self) {
+    pub(crate) fn parse(bytes: &[u8]) -> (usize, Self, DBStrings) {
         let mut offset: usize = 0;
 
         let entry_count: u32 = as_u32_le(&bytes[offset..offset+4]);
@@ -18,15 +21,24 @@ impl CSV {
 
         offset += 1; // padding
 
-        offset += 1; // String count, should always be 4
+        // I could not manually create a command with string count != 4, but I found a map 
+        // that has a command with string count 0, so we'll handle that case.
+        let string_count: u8 = bytes[offset];
+        offset += 1;
 
         let (bytes_read, filename): (usize, String) = parse_string(&bytes[offset..]);
         offset += bytes_read;
 
-        (offset, Self {
+        let (bytes_read, db_strings): (usize, DBStrings)
+            = State::parse_strings(string_count, &bytes[offset..]);
+        offset += bytes_read;
+
+        let state: Self = Self {
             entry_count,
             filename
-        })
+        };
+
+        (offset, state, db_strings)
     }
 
     pub fn entry_count(&self) -> u32 {
